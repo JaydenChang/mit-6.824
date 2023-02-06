7264c3f9273663495b4ed2ccac9ff1478de9b0b9
JaydenChang <zhang44378@163.com>
2023-02-06 13:10:20 +0800

    finish the all tasks

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,11 +6,38 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type Coordinator struct {
 	// Your definitions here.
+	// State            int // 0 map 1 reduce 2 finish
+	State          int32
+	MapTask        chan Task
+	ReduceTask     chan Task
+	NumMapTask     int
+	NumReduceTask  int
+	MapTaskTime    sync.Map
+	ReduceTaskTime sync.Map
+	// MapTaskFinish    chan bool
+	// ReduceTaskFinish chan bool
+	files []string
+	Mu    sync.RWMutex
+}
 
+type Task struct {
+	FileName string
+	MapID    int
+	ReduceID int
+	TaskType int32
+	// TaskTime TimeStamp
+}
+
+type TimeStamp struct {
+	TimeNow int64
+	Finish  bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,6 +50,103 @@ type Coordinator struct {
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
+}
+
+func (c *Coordinator) GetTask(args *TaskArgs, reply *TaskReply) error {
+	state := atomic.LoadInt32(&c.State)
+	if state == 0 {
+		if len(c.MapTask) != 0 {
+			mapTask, ok := <-c.MapTask
+			if ok {
+				reply.XTask = mapTask
+			}
+			reply.CurMapNum = len(c.MapTask)
+		} else {
+			reply.CurMapNum = -1
+		}
+		reply.CurReduceNum = len(c.ReduceTask)
+	} else if state == 1 {
+		if len(c.ReduceTask) != 0 {
+			reduceTask, ok := <-c.ReduceTask
+			if ok {
+				reply.XTask = reduceTask
+			}
+			reply.CurReduceNum = len(c.ReduceTask)
+		} else {
+			reply.CurReduceNum = -1
+		}
+		reply.CurMapNum = -1
+	}
+
+	reply.NumMapTask = c.NumMapTask
+	reply.NumReduceTask = c.NumReduceTask
+	reply.XTask.TaskType = state
+	return nil
+}
+
+func (c *Coordinator) FinishTask(args *Task, reply *TaskReply) error {
+	timeNow := time.Now().Unix()
+
+	if GetMapSize(&c.MapTaskTime) != c.NumMapTask {
+		startTime, _ := c.MapTaskTime.Load(args.MapID)
+		if timeNow-startTime.(TimeStamp).TimeNow > 10 {
+			return nil
+		}
+		c.MapTaskTime.Store(args.MapID, TimeStamp{time.Now().Unix(), true})
+		if GetMapSize(&c.MapTaskTime) == c.NumMapTask {
+			atomic.StoreInt32(&c.State, 1)
+			for i := 0; i < c.NumReduceTask; i++ {
+				c.ReduceTask <- Task{ReduceID: i}
+				c.ReduceTaskTime.Store(i, TimeStamp{time.Now().Unix(), false})
+			}
+		}
+	} else if GetMapSize(&c.ReduceTaskTime) != c.NumReduceTask {
+		startTime, _ := c.ReduceTaskTime.Load(args.ReduceID)
+		if timeNow-startTime.(TimeStamp).TimeNow > 10 {
+			return nil
+		}
+		c.ReduceTaskTime.Store(args.ReduceID, TimeStamp{time.Now().Unix(), true})
+		if GetMapSize(&c.ReduceTaskTime) == c.NumReduceTask {
+			atomic.StoreInt32(&c.State, 2)
+		}
+	}
+
+	return nil
+}
+
+func (c *Coordinator) TimeTick() {
+	state := atomic.LoadInt32(&c.State)
+	timeNow := time.Now().Unix()
+	if state == 0 {
+		for i := 0; i < c.NumMapTask; i++ {
+
+			tempTask, _ := c.MapTaskTime.Load(i)
+			if !tempTask.(TimeStamp).Finish && timeNow-tempTask.(TimeStamp).TimeNow > 10 {
+				c.MapTask <- Task{MapID: i, FileName: c.files[i]}
+				c.MapTaskTime.Store(i, TimeStamp{time.Now().Unix(), false})
+			}
+		}
+	} else if state == 1 {
+		for i := 0; i < c.NumReduceTask; i++ {
+			tempTask, _ := c.ReduceTaskTime.Load(i)
+			if !tempTask.(TimeStamp).Finish && timeNow-tempTask.(TimeStamp).TimeNow > 10 {
+				c.ReduceTask <- Task{ReduceID: i}
+				c.ReduceTaskTime.Store(i, TimeStamp{time.Now().Unix(), false})
+			}
+
+		}
+	}
+}
+
+func GetMapSize(m *sync.Map) int {
+	i := 0
+	m.Range(func(key, value interface{}) bool {
+		if value.(TimeStamp).Finish {
+			i++
+		}
+		return true
+	})
+	return i
 }
 
 //
@@ -46,8 +170,16 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
+	// ret := true
+	c.TimeTick()
 	ret := false
+	// fmt.Println("<<<<<<<<< ", GetMapSize(&c.ReduceTaskTime), c.NumReduceTask)
 
+	if GetMapSize(&c.ReduceTaskTime) == c.NumReduceTask {
+		ret = true
+	}
+
+	time.Sleep(time.Second)
 	// Your code here.
 
 	return ret
@@ -59,7 +191,26 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		NumMapTask:    len(files),
+		NumReduceTask: nReduce,
+		MapTask:       make(chan Task, len(files)),
+		ReduceTask:    make(chan Task, nReduce),
+		// MapTaskFinish:    make(chan bool, len(files)),
+		// ReduceTaskFinish: make(chan bool, nReduce),
+		files: files,
+		State: 0,
+	}
+
+	for id, file := range files {
+		c.MapTask <- Task{
+			FileName: file,
+			MapID:    id,
+			TaskType: 0,
+		}
+		c.MapTaskTime.Store(id, TimeStamp{time.Now().Unix(), false})
+
+	}
 
 	// Your code here.
 
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +20,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,7 +39,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -35,6 +49,108 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	for {
+		reply := TaskReply{}
+		args := TaskArgs{}
+		CallGetTask(&args, &reply)
+		filename := reply.XTask.FileName
+		// MapID := reply.MapID
+		// ReduceID := reply.ReduceID
+		state := reply.XTask.TaskType
+		// fmt.Println(">><<<<<<<<<<<<<<<<< state: ",state)
+		// if reply.FileName == "" && reply.XTask.TaskType == 0 {
+		// 	fmt.Println("><>>>>>>>>>>>>>>>>>>> ",reply.NumMapTask,reply.XTask.MapID)
+		// }
+		if state == 0 && reply.CurMapNum >= 0 {
+			nReduce := reply.NumReduceTask
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open file %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+
+			kva := mapf(filename, string(content))
+
+			bucket := make([][]KeyValue, nReduce)
+			for _, kv := range kva {
+				num := ihash(kv.Key) % nReduce
+				bucket[num] = append(bucket[num], kv)
+
+			}
+			for i := 0; i < nReduce; i++ {
+				tempFile, err := ioutil.TempFile("", "mr-map-*"+strconv.Itoa(i))
+				if err != nil {
+					log.Fatalf("Could not create temporary file")
+				}
+				enc := json.NewEncoder(tempFile)
+				for _, kv := range bucket[i] {
+					err := enc.Encode(&kv)
+					if err != nil {
+						log.Fatalf("Could not encode")
+					}
+				}
+				os.Rename(tempFile.Name(), "mr-"+strconv.Itoa(reply.XTask.MapID)+"-"+strconv.Itoa(i))
+				tempFile.Close()
+			}
+			CallFinishTask(&reply.XTask, &reply)
+		} else if state == 1 && reply.CurReduceNum >= 0 {
+			NumMapTask := reply.NumMapTask
+			ReduceID := reply.XTask.ReduceID
+			intermediate := []KeyValue{}
+
+			for i := 0; i < NumMapTask; i++ {
+				intermediateName := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(ReduceID)
+				intermediateFile, err := os.Open(intermediateName)
+				if err != nil {
+					log.Fatalf("Could not open intermediate file")
+				}
+				dec := json.NewDecoder(intermediateFile)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						break
+					}
+					intermediate = append(intermediate, kv)
+				}
+			}
+			// if ReduceID == 0 {
+			// 	fmt.Println("$$$$$$$$$$ using reduce id 0", ReduceID)
+			// }
+			sort.Sort(ByKey(intermediate))
+			oname := "mr-temp-" + strconv.Itoa(ReduceID)
+			tempReduceFile, err := ioutil.TempFile("", oname)
+			if err != nil {
+				log.Fatalf("Could not create temporary file")
+			}
+
+			i := 0
+			for i < len(intermediate) {
+				j := i + 1
+				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+					j++
+				}
+				values := []string{}
+				for k := i; k < j; k++ {
+					values = append(values, intermediate[k].Value)
+				}
+				output := reducef(intermediate[i].Key, values)
+
+				fmt.Fprintf(tempReduceFile, "%v %v\n", intermediate[i].Key, output)
+				i = j
+
+			}
+			tempReduceFile.Close()
+			os.Rename(tempReduceFile.Name(), "mr-out-"+strconv.Itoa(ReduceID))
+			CallFinishTask(&reply.XTask, &reply)
+		} else if state == 2 {
+			break
+		}
+		time.Sleep(time.Second)
+	}
 
 }
 
@@ -64,6 +180,24 @@ func CallExample() {
 		fmt.Printf("reply.Y %v\n", reply.Y)
 	} else {
 		fmt.Printf("call failed!\n")
+	}
+}
+
+func CallGetTask(args *TaskArgs, reply *TaskReply) {
+	// args := TaskArgs{}
+	// args.Msg = "hello\n"
+	// reply := TaskReply{}
+	ok := call("Coordinator.GetTask", &args, &reply)
+
+	if !ok {
+		fmt.Printf("call get task failed!\n")
+	}
+}
+func CallFinishTask(args *Task, reply *TaskReply) {
+	ok := call("Coordinator.FinishTask", &args, &reply)
+
+	if !ok {
+		fmt.Printf("call finish failed!\n")
 	}
 }
 
